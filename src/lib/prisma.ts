6346647e6e610d6813a27dcb712787dbe2808730
u6lqre <ozwtn9@tutamail.com>
2026-02-10 12:31:49 +0100

    fix: set up correctly prisma and sqlite

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from "generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
-const adapter = new PrismaBetterSqlite3({ connectionString });
+const adapter = new PrismaBetterSqlite3({ url: connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
