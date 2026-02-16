7a6fe0a2d1b90f325f365956d15f953985a4424f
u6lqre <ozwtn9@tutamail.com>
2026-02-16 18:43:04 +0100

    chore: install cors pkg

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { authRouter } from "./routes/auth.routes";
+import cors from "cors";
 
 const app = express();
 const port: number = 3000;
 
 app.use(express.json());
-
+app.use(cors());
 app.use(authRouter);
 
 app.listen(port, () => {
